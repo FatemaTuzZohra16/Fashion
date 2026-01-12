15a949a38a51b039b1e416480befbdd7af0a6b53
FatemaTuzZohra16 <aneyfatema7@gmail.com>
2026-01-12 17:16:39 +0600

    implement Redux store and slices for eCommerce state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './Components/Pages/Home'
+import store from './store'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
      <Home />
-    </>
+    </Provider>
   )
 }
 
